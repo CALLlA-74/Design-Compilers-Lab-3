74af811c26ec821e34e7595aa7a3b3de89ae5ca7
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2024-05-25 08:28:15 +0300

    Lab 3

---
+++ main.py
@@ -0,0 +1,16 @@
+from grammar_parser import tokenize, parse
+modes = ["parse_expression", "parse_program"]
+
+code = '''
+begin 
+    v := not(-(+2.4 + 3/2)) <> not (100.0/(a + b)-28*0.001);
+    a:=2/4;
+    b :=a;
+    c := not (100.0/(25 + 46)-28*0.001)
+end
+'''
+
+if __name__ == '__main__':
+    tokens = tokenize(code)
+    print(tokens)
+    parse(modes[1], tokens)
